496915400eba8a4a677f1741b84f52c15fb52327
syifamahira <dharmawansyifa@gmail.com>
2024-05-17 20:45:54 +0700

    fix: templates

--- fitur_pink/views.py
+++ fitur_pink/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+def create_album(request):
+    return render(request, 'create_album.html')
+def list_album(request):
+    return render(request, 'list_album.html')
+def create_song(request):
+    return render(request, 'create_song.html')
+def list_songs_in_album(request):
+    return render(request, 'list_songs_in_album.html')
+def cek_royalty(request):
+    return render(request, 'cek_royalty.html')
+def create_podcast(request):
+    return render(request, 'create_podcast.html')
+def list_podcast(request):
+    return render(request, 'list_podcast.html')
+def create_episode(request):
+    return render(request, 'create_episode.html')
+def list_episodes_in_podcast(request):
+    return render(request, 'list_episodes_in_podcast.html')
+def list_album_label(request):
+    return render(request, 'list_album_label.html')
+def list_songs_in_album_label(request):
+    return render(request, 'list_songs_in_album_label.html')
