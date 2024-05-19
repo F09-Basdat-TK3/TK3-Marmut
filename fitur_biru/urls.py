2829bc21874fd7da1fd78a7b4de4aa28c50655eb
Maulana Seto <maulana.seto@ui.ac.id>
2024-05-19 10:46:41 +0700

    Changed Function Name

--- fitur_biru/urls.py
+++ fitur_biru/urls.py
@@ -4,7 +4,7 @@ from fitur_biru.views import *
 app_name = 'fitur_biru'
 
 urlpatterns = [
-    path('', show_podcaster, name='show_podcaster'),
+    path('', show_podcasters, name='show_podcasters'),
     path('show-podcasts/<str:podcaster_email>', show_podcasts, name="show-podcasts"),
     path('show-episodes/<str:id_konten>', show_episodes, name='show-episodes'),
 ]
--- fitur_biru/views.py
+++ fitur_biru/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 import uuid
 
-def show_podcaster(request):
+def show_podcasters(request):
     podster = query("SELECT A.nama, A.email FROM AKUN A JOIN PODCASTER PR ON A.email = PR.email")
     context = {'podcasters': podster}
     return render(request, "podcaster-list.html", context)
